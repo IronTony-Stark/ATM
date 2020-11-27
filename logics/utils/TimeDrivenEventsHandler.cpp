6a44fb20d99610c2d7f698af61441eff79352c59
BlaD200 <blad600@gmail.com>
2020-11-27 18:07:05 +0200

    [Fix] payment from blocked or unknown card. [Fix] deposit increases.

--- logics/utils/TimeDrivenEventsHandler.cpp
+++ logics/utils/TimeDrivenEventsHandler.cpp
@@ -9,12 +9,27 @@
 #include "QDebug"
 
 
-void TimeDrivenEventsHandler::payPayments() {
+void TimeDrivenEventsHandler::payPayments(const QDateTime& dateTime) {
     qDebug() << "Paying for payments";
     const QList<RegularPayment*>& payments = PaymentDAO::getInstance().getAll();
+    const CardDAO& cardDao = CardDAO::getInstance();
     for (RegularPayment* payment: payments) {
-        // TODO check for card was blocked
-        if (payment->dayOfMonth() == QDateTime::currentDateTime().date().day()) {
+        Card* senderCard = cardDao.getById(payment->sender());
+        Card* receiverCard = cardDao.getById(payment->receiver());
+        if (senderCard == nullptr){
+            qWarning() << "Card " << payment->sender() << "wasn't found for payment " << payment->id();
+        }
+        if (receiverCard == nullptr){
+            qWarning() << "Card " << payment->receiver() << "wasn't found for payment " << payment->id();
+        }
+        if (receiverCard == nullptr || senderCard == nullptr)
+            continue;
+
+        if (senderCard->isBlocked() || receiverCard->isBlocked()){
+            qInfo() << "Skipped payment " << payment->id() << " because of blocked card";
+            continue;
+        }
+        if (payment->dayOfMonth() == dateTime.date().day()) {
             OperationManager::transfer(payment->sender(), payment->receiver(), payment->amount());
         }
     }
@@ -24,10 +39,16 @@ void TimeDrivenEventsHandler::payPayments() {
 void TimeDrivenEventsHandler::onTimeChanged(const QDateTime& dateTime) {
     if (_isPayed && dateTime.time() <= QTime(0, 1))
         _isPayed = false;
-    if (!_isPayed && dateTime.time() >= QTime(10, 0))
-        payPayments();
-    increaseDeposits(dateTime);
-    qDebug() << "click " << dateTime.time().second();
+    if (dateTime.time().minute() == 1)
+        _isIncreased = false;
+    if (!_isPayed && dateTime.time() >= QTime(10, 0)) {
+        checkPaymentsDate(dateTime);
+        payPayments(dateTime);
+    }
+    if (!_isIncreased && dateTime.time().minute() == 0)
+        increaseDeposits(dateTime);
+
+    qDebug() << "minute " << dateTime.time().minute();
 }
 
 void TimeDrivenEventsHandler::increaseDeposits(const QDateTime& dateTime) {
@@ -38,10 +59,25 @@ void TimeDrivenEventsHandler::increaseDeposits(const QDateTime& dateTime) {
         qDebug() << deposit->name() << ": " << deposit->endDate();
         if (deposit->endDate() >= dateTime.date()) {
             double interestPerDay = deposit->interest() / 365;
-            const Money& incomePerDay = deposit->sum() * interestPerDay;
-            qDebug() << deposit->name() << ": " << incomePerDay.operator double();
+            const Money& incomePerDay = (double)deposit->sum() * interestPerDay;
+            qDebug() << deposit->name() << ": " << (double)incomePerDay;
             deposit->replenish(incomePerDay);
             depositDao.updateDeposit(*deposit);
+        }
+    }
+    _isIncreased = true;
+}
+
+void TimeDrivenEventsHandler::checkPaymentsDate(const QDateTime& dateTime) {
+    qDebug() << "Checking payments date";
+    PaymentDAO& paymentDao = PaymentDAO::getInstance();
+    const QList<RegularPayment*>& payments = paymentDao.getAll();
+    for (RegularPayment* payment: payments) {
+        int daysInMonth = dateTime.date().daysInMonth();
+        if (payment->dayOfMonth() > daysInMonth || daysInMonth < 31) {
+            payment->setDayOfMonth(31 - daysInMonth);
+            paymentDao.removePayment(payment->id());
+            paymentDao.save(*payment);
         }
     }
 }
